9708ddfae0d5ca44e7867d244549fa9d20963982
yyang5823 <yyang5823@gmail.com>
2016-05-02 16:24:57 -0400

    "add second file"

---
+++ add_reg_key.ps1
@@ -0,0 +1,15 @@
+﻿# initiation section
+$key = "PSBlackjack"
+$value = "Credits"
+$data = "true"
+$type = "string"
+
+#functions and filters section
+function Create-KeyAndValue {
+    New-Item -name $key   #create new reg key
+    New-ItemProperty $key -name $value -Type $type -value $data
+}
+
+# Main processing section
+set-location hkcu:\
+Create-KeyAndValue
