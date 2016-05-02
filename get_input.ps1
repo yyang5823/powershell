0c510b48ffc9efea1dbb26c2dc5c94ba46c328a6
Yifang Yang <yyang4@ford.com>
2016-05-02 15:09:30 -0400

    initial commit

---
+++ get_input.ps1
@@ -0,0 +1,38 @@
+﻿#clear the console window
+
+clear-host
+
+#declare variables to store answers
+$ProjectName = ""
+$Platform = ""
+$AdminName =""
+$LaunchDate = ""
+
+#start display question
+Write-host "Questions and Answers Session"
+Write-host "Please press a key to continue"
+Read-host
+
+while ($ProjectName -eq "") {
+    $ProjectName = read-host "What is your project name?"
+}
+
+while ($Platform -eq "") {
+    $Platform = read-host "What is your project platform?"
+}
+
+while ($AdminName -eq "") {
+    $AdminName = read-host "Who is your project administrator?"
+}
+
+while ($LaunchDate -eq "") {
+    $LaunchDate = read-host "Please provide planned launch date?"
+}
+
+Write-host
+Write-host "Here are the summary of collected info from you:"
+Write-host "ProjectName: $ProjectName"
+Write-host "Platform: $Platform"
+Write-host "Project Admin: $AdminName"
+Write-host "Launch Date: $LaunchDate"
+Write-host "Thank you for the input"
