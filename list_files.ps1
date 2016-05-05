6e9e89bdf220c29225ce11b396ec0e9c98f9e144
yyang5823 <yyang5823@gmail.com>
2016-05-05 12:59:58 -0400

    "removed list_files.ps1"

--- list_files.ps1
+++
@@ -1,8 +0,0 @@
-﻿# simple function to sort a list of items
-function list-items {
-    $input | Where-Object {$_.Name -ne "Users"} | Sort-Object
-}
-
-set-location $args[0]
-Write-host "input args: $args[0]"
-Get-ChildItem |list-items
